27a50253fbda66308c49c0124279fda226b1595a
WyvernH <93463398+WyvernH@users.noreply.github.com>
2024-03-08 00:38:48 -0800

    fundamental list of stackops

--- include/builtins/stackops.h
+++ include/builtins/stackops.h
@@ -20,13 +20,13 @@ void cog_dsc(value_t *v);
 /* void cog_dscn(value_t *v); */
 
 /* discarding values further down the stack */
-void cog_nip(value_t *v); // dscd
+/* void cog_nip(value_t *v); // dscd */
 /* void cog_nip2(value_t *v); */
 /* void cog_nip3(value_t *v); */
 /* void cog_nip4(value_t *v); */
 /* void cog_nipn(value_t *v); */
 
-void cog_nipd(value_t *v); // dscdd
+/* void cog_nipd(value_t *v); // dscdd */
 /* void cog_nipd2(value_t *v); */
 /* void cog_nipd3(value_t *v); */
 /* void cog_nipd4(value_t *v); */
