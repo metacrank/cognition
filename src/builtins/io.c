232d5a68161af14f474e5d7cf8fc3f23b0b94568
WyvernH <93463398+WyvernH@users.noreply.github.com>
2024-03-09 11:43:32 -0800

    fixed bugs, wrote helloworld.cog

--- src/builtins/io.c
+++ src/builtins/io.c
@@ -17,11 +17,40 @@ void cog_questionmark(value_t *v) {
 void cog_period(value_t *v) {
   contain_t *cur = stack_peek(STACK);
   value_t *elt = stack_pop(cur->stack);
+  if (elt == NULL) {
+    eval_error("EMPTY STACK", v);
+    return;
+  }
   print_value(elt, "\n");
   value_free(elt);
 }
 
+void cog_print(value_t *v) {
+  contain_t *cur = stack_peek(STACK);
+  value_t *v1 = stack_pop(cur->stack);
+  if (v1 == NULL) {
+    eval_error("EMPTY STACK", v);
+    return;
+  }
+  if (v1->container->stack->size == 0) {
+    stack_push(cur->stack, v1);
+    eval_error("TYPE ERROR", v);
+    return;
+  }
+  value_t *word = v1->container->stack->items[0];
+  if (word->type != VWORD) {
+    stack_push(cur->stack, v1);
+    eval_error("TYPE ERROR", v);
+    return;
+  }
+  printf("%s", word->str_word->value);
+  value_free(v1);
+}
+
+void cog_read(value_t *v) {}
+
 void add_funcs_io(ht_t* flit) {
   add_func(flit, cog_questionmark, "?");
   add_func(flit, cog_period, ".");
+  add_func(flit, cog_print, "print");
 }
